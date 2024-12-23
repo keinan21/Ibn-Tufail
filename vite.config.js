a6f6e4b94894e5026e90595a18cf6b09814b21bb
Muhammad Yusuf Sintara <142227388+keinan21@users.noreply.github.com>
2024-12-23 21:46:46 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
+ build: {
+    minify: false,
+    cssCodeSplit: true,
+    modulePreload: false,
+  },
  base: '/vite-deploy-demo/'
 })
